4decb05114f9f2a5f57e2a3ca6a4ce5086def96f
Desktop <no@no.com>
2019-02-12 15:39:09 +0100

    Gun render and movement

---
+++ Assets/Scripts/Player/VR_Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace Assets.Scripts.Player
+{
+    /// <summary>
+    /// Set the different button presses of a VR controller.
+    /// </summary>
+    public class VR_Controller : MonoBehaviour
+    {
+        public SteamVR_Input_Sources handType;
+        public SteamVR_Action_Boolean triggerAction;
+
+        public bool GetTrigger()
+        {
+            return triggerAction.GetState(handType);
+        }
+
+        public void Update()
+        {
+            bool value = GetTrigger();
+            if (value) print("Trigger state:" + value);
+        }
+    }
+}
