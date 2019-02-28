21df999923af566053ae72df36a440584d988a0d
TEAMVR038 <teamvr038@gmail.com>
2019-02-28 14:53:36 +0100

    Added scene loading and ingame gun transform finetune

---
+++ Assets/Scripts/ApplyGunRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+class ApplyGunRotation : MonoBehaviour
+{
+    public Gun gun;
+    public bool normalGun = false;
+
+    void Start()
+    {
+        if (normalGun)  ApplyRealGunRotation();
+        else            ApplyControllerRotation();
+    }
+
+    private void ApplyControllerRotation()
+    {     
+        gun.transform.rotation = Quaternion.Euler(-50, -180, 0);
+        gun.transform.position = new Vector3(0, -0.05f, -0.05f);
+    }
+
+    // TODO
+    private void ApplyRealGunRotation()
+    {
+        gun.transform.rotation = Quaternion.Euler(0, 0, 0);
+        gun.transform.position = new Vector3();
+    }
+}
