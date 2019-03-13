b0f171ff111dcc16dd7bea770ce03adabab78c80
TEAMVR038 <teamvr038@gmail.com>
2019-03-13 13:54:27 +0100

    Added shooting range transition

---
+++ Assets/Scripts/Utils/CityMatrixEffect.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class CityMatrixEffect : MonoBehaviour
+{
+    public void EmergeFromGround()
+    {
+        DOTween.To(() => transform.position, pos => transform.position = pos, new Vector3(0, 0, 0), 5);
+    }
+}
