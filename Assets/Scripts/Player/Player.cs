cc281e3f195daddc87143abefb6dd0921f0fbd19
Maciej Olejnik <maciek.olejnik@tlen.pl>
2019-03-19 14:25:36 +0100

    Attached a gun to NPCs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,63 +1,60 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace Assets.Scripts.Player
+/// <summary>
+/// Saves data relevant to the player.
+/// </summary>
+public class Player : MonoBehaviour, IHitable
 {
-    /// <summary>
-    /// Saves data relevant to the player.
-    /// </summary>
-    public class Player : MonoBehaviour, IHitable
+    public Vector3 playerHeight = new Vector3(0, 1.64f);
+    public static Player Instance { get; private set; }
+
+    public VR_Controller leftHandInput, rightHandInput;
+    public Gun leftGun, rightGun;
+    private GunInterface leftHand, rightHand;
+
+    public Hitbox hitbox;
+
+    private void Start()
     {
-        public Vector3 playerHeight = new Vector3(0, 1.64f);
-        public static Player Instance { get; private set; }
+        if (leftGun != null) leftHand = new GunInterface(leftGun);
+        if (rightGun != null) rightHand = new GunInterface(rightGun);
+    }
 
-        public VR_Controller leftHandInput, rightHandInput;
-        public Gun leftGun, rightGun;
-        private GunInterface leftHand, rightHand;
+    private void Awake()
+    {
+        Instance = this;
+    }
 
-        public Hitbox hitbox;
-
-        private void Start()
+    public void Update()
+    {
+        if (leftHandInput != null && leftHand != null)
         {
-            if (leftGun != null) leftHand = new GunInterface(leftGun);
-            if (rightGun != null) rightHand = new GunInterface(rightGun);
+            leftHand.HandleInput(leftHandInput);
+            leftGun.gameObject.SetActive(leftHandInput.IsControllerWorking());
         }
 
-        private void Awake()
+        if (rightHandInput != null && rightHand != null)
         {
-            Instance = this;
+            rightHand.HandleInput(rightHandInput);
+            rightGun.gameObject.SetActive(rightHandInput.IsControllerWorking());
         }
+    }
 
-        public void Update()
+    public void OnHit(BulletHit bulletHit)
+    {
+        Debug.Log("PLAYER WAS SHOT");
+    }
+
+    void OnGUI()
+    {
+        IList<LoggedHit> hits = Scenario.GetHits();
+
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "Shot count: " + (leftGun.shotsFired + rightGun.shotsFired));
+
+        for (int i = 0; i < hits.Count; i++)
         {
-            if (leftHandInput != null)
-            {
-                leftHand.HandleInput(leftHandInput);
-                leftGun.gameObject.SetActive(leftHandInput.IsControllerWorking());
-            }
-
-            if (rightHandInput != null)
-            {
-                rightHand.HandleInput(rightHandInput);
-                rightGun.gameObject.SetActive(rightHandInput.IsControllerWorking());
-            }
-        }
-
-        public void OnHit(BulletHit bulletHit)
-        {
-            Debug.Log("PLAYER WAS SHOT");
-        }
-
-        void OnGUI()
-        {
-            IList<LoggedHit> hits = Scenario.GetHits();
-
-            GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "Shot count: " + (leftGun.shotsFired + rightGun.shotsFired));
-
-            for (int i = 0; i < hits.Count; i++)
-            {
-                GUI.Label(new Rect(Screen.width / 12, Screen.height / 24 * i, Screen.width / 4 * 2, Screen.height / 6), hits[i].part.ToString());
-            }
+            GUI.Label(new Rect(Screen.width / 12, Screen.height / 24 * i, Screen.width / 4 * 2, Screen.height / 6), hits[i].part.ToString());
         }
     }
 }
