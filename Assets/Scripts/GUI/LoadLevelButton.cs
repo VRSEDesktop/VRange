7e16df8d32ae089eadb9707fbe0aca87a0746913
Maciej Olejnik <maciek.olejnik@tlen.pl>
2019-03-21 11:58:45 +0100

    Buttons test

---
+++ Assets/Scripts/GUI/LoadLevelButton.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using Valve.VR;
+
+public class LoadLevelButton : MonoBehaviour, IHitable
+{
+    public string levelName;
+
+    public void Start()
+    {
+        GetComponentInChildren<TextMeshPro>().text = name;
+    }
+
+    public void OnHit(BulletHit bulletHit)
+    {
+        GetComponent<SteamVR_LoadLevel>().levelName = levelName;
+        GetComponent<SteamVR_LoadLevel>().Trigger();
+    }
+}
--- Assets/Scripts/Scenarios/MainMenu.cs
+++ Assets/Scripts/Scenarios/MainMenu.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Scenarios
+public class MainMenu : MonoBehaviour
 {
-    class MainMenu
+    private void HandleShootingRangeButton()
     {
+
     }
 }
