8614e501a7c35d6e686311ef74a67655c0002590
Maciej Olejnik <maciek.olejnik@tlen.pl>
2019-04-03 13:51:40 +0100

    Begin of sight loader

---
+++ Assets/Scripts/GUI/SightLoader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class SightLoader : MonoBehaviour
+{
+    private static readonly int layer = LayerMask.NameToLayer("UI");
+    private static readonly int maxRange = 50;
+    private static readonly float timeToLoad = 3f;
+
+    /// <summary>
+    /// Object which was in sight line in the last frame
+    /// </summary>
+    private GameObject lastObject;
+
+    /// <summary>
+    /// Time player is looking at specific UI object
+    /// </summary>
+    private float timer = 0;
+
+    public void Update()
+    {
+        bool hasHit = Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxRange, layer);
+
+        if (hasHit && hit.collider.gameObject.GetComponent<ISightActivable>() != null)
+        {
+            ISightActivable uiElement = hit.collider.gameObject.GetComponent<ISightActivable>();
+
+            Debug.Log(hit.collider.transform.name);
+
+            if (lastObject.Equals(hit.collider.gameObject)) timer += Time.deltaTime;
+            else timer = 0;
+
+            lastObject = hit.collider.gameObject;
+
+            if (timer >= timeToLoad) uiElement.Activate();
+            uiElement.Draw();
+        }
+        else
+        {
+            lastObject = null;
+            timer = 0;
+        }
+    }
+}
