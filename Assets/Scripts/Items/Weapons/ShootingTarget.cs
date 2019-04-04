d278eac555de70f30c137a29c5c3fcbc305d7727
TEAMVR038 <teamvr038@gmail.com>
2019-04-04 10:41:59 +0100

    Fixed the first exercise
    The flipping cardboard works now

--- Assets/Scripts/Items/Weapons/ShootingTarget.cs
+++ Assets/Scripts/Items/Weapons/ShootingTarget.cs
@@ -3,6 +3,7 @@
 public class ShootingTarget : MonoBehaviour, IHitable
 {
     public Hitbox[] hitboxes;
+    public HitboxType Goal;
 
     public HitType OnHit(Gun gun, RaycastHit raycastHit)
     {
@@ -10,20 +11,24 @@ public class ShootingTarget : MonoBehaviour, IHitable
 
         Debug.Log("ShootingTarrget::OnHit() " + partHit.ToString());
         Scenario.logs.Add(new LoggedHit(this, partHit, gun, raycastHit));
-
+        if(partHit == Goal)
+        {
+            return HitType.RIGHT;
+        }
         switch (partHit)
         {
-            case HitboxType.TARGET_HEAD:
-            case HitboxType.TARGET_ARM_LEFT:
-            case HitboxType.TARGET_ARM_RIGHT:
-            case HitboxType.TARGET_LEG_LEFT:
-            case HitboxType.TARGET_LEG_RIGHT:
-            case HitboxType.TARGET_HAND_LEFT:
-            case HitboxType.TARGET_HAND_RIGHT:
+            case HitboxType.Hoofd:
+            case HitboxType.Linkerarm:
+            case HitboxType.Rechterarm:
+            case HitboxType.Linkerbeen:
+            case HitboxType.Rechterbeen:
+            case HitboxType.Linkerhand:
+            case HitboxType.Rechterhand:
+            case HitboxType.Torso:
             case HitboxType.TARGET_TORSO_7:
             case HitboxType.TARGET_TORSO_8:
             case HitboxType.TARGET_TORSO_9:
-            case HitboxType.TARGET_TORSO_10: return HitType.RIGHT;
+            case HitboxType.TARGET_TORSO_10: return HitType.UNWANTED;
         }
 
         return HitType.MISS;
