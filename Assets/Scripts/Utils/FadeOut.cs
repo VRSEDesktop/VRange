efb145d07b571bb1cf475bbcf9a7995230f24663
Lucas <pinkypiedude@gmail.com>
2019-05-06 11:31:27 +0200

    Shouldn't fade out after exercise ends.

--- Assets/Scripts/Utils/FadeOut.cs
+++ Assets/Scripts/Utils/FadeOut.cs
@@ -4,16 +4,61 @@ using UnityEngine;
 
 public class FadeOut : MonoBehaviour
 {
-    public void StartFadeOut(float delay)
+	public bool ForceEnable = false;
+
+	private Renderer Renderer;
+	private IEnumerator Coroutine;
+
+	public void OnEnable()
 	{
-		IEnumerator routine = DisableLine(delay);
-		if(gameObject.activeSelf)
-			StartCoroutine(routine);
+		Renderer = gameObject.GetComponent<Renderer>();
 	}
 
-	private IEnumerator DisableLine(float delay)
+	public void Update()
+	{
+		if (ForceEnable && Coroutine != null)
+			StopCoroutine(Coroutine);
+	}
+
+    public void StartFadeOut(float delay)
+	{
+		Coroutine = DisableLine(delay, 2);
+		if(gameObject.activeSelf)
+			StartCoroutine(Coroutine);
+	}
+
+	private IEnumerator DisableLine(float delay, float fadeoutduration)
 	{
 		yield return new WaitForSeconds(delay);
+
+		float lerpStarttime = Time.time;
+		float lerpProgress;
+		Color startingColor = Renderer.material.GetColor("_BaseColor");
+		Color endingColor = new Color(startingColor.r, startingColor.g, startingColor.b, 0);
+		while(true)
+		{
+			yield return new WaitForEndOfFrame();
+			lerpProgress = Time.time - lerpStarttime;
+			if(Renderer != null)
+			{
+				Renderer.material.SetColor("_BaseColor", Color.Lerp(startingColor, endingColor, lerpProgress / fadeoutduration));
+			}
+			else
+			{
+				break;
+			}
+
+			if (lerpProgress >= fadeoutduration)
+				break;
+
+			if(ForceEnable)
+			{
+				Renderer.material.SetColor("_BaseColor", startingColor);
+				break;
+			}
+		}
+		//So we don't have to set the color back when reenabling.
+		Renderer.material.SetColor("_BaseColor", startingColor);
 		gameObject.SetActive(false);
 	}
 }
